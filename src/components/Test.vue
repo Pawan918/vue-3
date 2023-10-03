9b1760f74cd40dff73ca8d689c0ddb0e313227f0
Pawan918 <pawanshah84489@gmail.com>
2023-10-03 11:10:33 +0530

    Dispalying name

---
+++ src/components/Test.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="testing">
+        {{ name }}
+    </div>
+</template>
+<script>
+export default {
+    name: 'TestComp',
+    data (){
+        return {
+            name : 'Pawan Kumar',
+        }
+    }
+}
+</script>
