1f65661bde32d9609f57a19617da17da154bac5f
Pawan918 <pawanshah84489@gmail.com>
2023-10-04 12:56:47 +0530

    slot assignment

---
+++ src/components/Card.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="">
+        <slot name="image"></slot>
+        <slot name="title"></slot>
+        <slot name="footer"></slot>
+    </div>
+</template>
--- src/components/Test.vue
+++ src/components/Test.vue
@@ -1,48 +1,32 @@
 <template>
     <div class="">
-        <Timer :count="count"></Timer>
-        <Control
-            @start-comp="start"
-            @stop-comp = 'stop'
-        />
+        <Card>
+            <template #image>
+                Image
+            </template>
+            <template #title>
+                <p>Title</p>
+            </template>
+            <template #footer>
+                <button>Footer</button>
+            </template>
+        </Card>
     </div>
 </template>
 <script >
-import Timer from './Timer.vue';
-import Control from './control.vue';
+// import Timer from './Timer.vue';
+// import Control from './control.vue';
 
 // import BaseButton from './baseButton.vue';
 // import Form from './Form.vue';
 // import List from './List.vue';
-
+import Card from './Card.vue'
 export default {
     name: "testTemp",
-    data(){
-        return{
-            count: 0,
-            timer : false,
-            timerFn:null,
-            
-        }
-    },
-    components: { Timer, Control },
-    methods : {
-        start(){
-            this.timer = true;
-            // this.count++;
-           this.timerFn = setInterval(this.increaseFn
-            ,1000);
-            // console.log('hello')
-        },
-        stop(){
-            this.timer = false;
-            clearTimeout(this.timerFn);
-            console.log('clear Timeout is called')
-        },
-        increaseFn(){
-            this.count++;
-        }
+    components : {
+        Card
     }
+
 }
 
 </script>
